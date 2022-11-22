61bf015c3c21ab93b4f2fa9fbda5d2ff523524cf
DHenox <DHenox@github.com>
2022-11-22 12:06:42 +0100

    First commit

---
+++ src/edu/upc/epsevg/prop/othello/players/hellowda/Hellowda.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.upc.epsevg.prop.othello.players.hellowda;
+
+/**
+ *
+ * @author HENOK
+ */
+public class Hellowda implements IPlayer, IAuto {
+    
+}
