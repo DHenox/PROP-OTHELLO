192c4f66f15633959811919d9e768a3ad5a1ac19
Almogawer <arnau.roca.grange@estudiantat.upc.edu>
2022-12-07 18:57:47 +0100

    heuristica mig mig

--- src/edu/upc/epsevg/prop/othello/players/hellouda/PlayerMiniMax.java
+++ src/edu/upc/epsevg/prop/othello/players/hellouda/PlayerMiniMax.java
@@ -10,7 +10,6 @@ import edu.upc.epsevg.prop.othello.SearchType;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.Random;
-import com.sun.tools.javac.util.Pair;
 import java.util.ArrayList; 
 
 /**
@@ -22,6 +21,7 @@ public class PlayerMiniMax implements IPlayer, IAuto {
     private String name;
     private GameStatus s;
     private CellType myType;
+    private CellType hisType;
 
     public PlayerMiniMax(String name) {
         this.name = name;
@@ -52,53 +52,67 @@ public class PlayerMiniMax implements IPlayer, IAuto {
     public Move move(GameStatus s) {
         
         myType = s.getCurrentPlayer();
-        Point mov = MiniMax(s, 4, true).snd;
+        hisType = CellType.opposite(myType);
+        Point mov = triaPosició(s, 4);
         return new Move( mov, 0L, 0, SearchType.RANDOM);
         //return move (posicio, 0, 0, MINIMAX)
     }
     
-    Pair<Integer, Point> MiniMax(GameStatus s, int depth, boolean maximizingPlayer){
-        //es terminal if isGameOver()
-            //hi ha un guanyador (s.checkGameOver() i GetWinner()
-            //no hi ha moviments possibles else
-        //la profunditat es 0
-        if(s.checkGameOver()){ //ha guanyat algu
-            if(myType == s.GetWinner())               //  Guanyem nosaltres
-                return new Pair<>(1000000, null);
-            else                                    //  Guanya el contrincant
-                return new Pair<>(-1000000, null);
+    Point triaPosició(GameStatus s, int depth){
+
+        int maxEval = Integer.MIN_VALUE;
+        Point bestMove = new Point();
+        ArrayList<Point> moves = s.getMoves();
+        for (int i = 0; i < moves.size(); i++) {
+            GameStatus fill = new GameStatus(s);
+            fill.movePiece(moves.get(i));
+            int eval = minValor(fill, depth-1);
+            if(maxEval < eval){
+                maxEval = eval;
+                bestMove = moves.get(i);
+            }
         }
-        else if(s.isGameOver() || depth==0){ //no hi ha moviments possibles o profunditat es 0
+        return bestMove;
+    }
+
+    int minValor(GameStatus s, int depth){
+        if(s.checkGameOver()){                  //ha guanyat algu
+            if(myType == s.GetWinner())             //  Guanyem nosaltres
+                return 1000000;
+            else                                    //  Guanya el contrincant
+                return -1000000;
+        }
+        else if(s.isGameOver() || depth==0){    //no hi ha moviments possibles o profunditat es 0
             return heuristica(s);
         }
-        Point pos = new Point();
+        int minEval = Integer.MAX_VALUE;
         ArrayList<Point> moves = s.getMoves();
-        if(maximizingPlayer){
-            int maxEval = Integer.MIN_VALUE;
-            for (int i = 0; i < moves.size(); i++) {
-                GameStatus fill = new GameStatus(s);
-                fill.movePiece(moves.get(i));
-                int eval = MiniMax(fill, depth-1, !maximizingPlayer);
-                if(maxEval < eval){
-                    maxEval = eval;
-                    pos = moves.get(i);
-                }
-                return new Pair<>(maxEval, pos);
-            }
+        for (int i = 0; i < moves.size(); i++) {
+            GameStatus fill = new GameStatus(s);
+            fill.movePiece(moves.get(i));
+            minEval = Math.min(minEval, maxValor(fill, depth-1));
         }
-        else{
-            int minEval = Integer.MAX_VALUE;
-            for (int i = 0; i < moves.size(); i++) {
-                GameStatus fill = new GameStatus(s);
-                fill.movePiece(moves.get(i));
-                int eval = MiniMax(fill, depth-1, !maximizingPlayer);
-                if(minEval > eval){
-                    minEval = eval;
-                    pos = moves.get(i);
-                }
-                return new Pair<>(minEval, pos);
-            }
+        return minEval;
+    }
+
+    int maxValor(GameStatus s, int depth){
+        if(s.checkGameOver()){                  //ha guanyat algu
+            if(myType == s.GetWinner())             //  Guanyem nosaltres
+                return 1000000;
+            else                                    //  Guanya el contrincant
+                return -1000000;
         }
+        else if(s.isGameOver() || depth==0){    //no hi ha moviments possibles o profunditat es 0
+            return heuristica(s);
+        }
+        int maxEval = Integer.MAX_VALUE;
+        ArrayList<Point> moves = s.getMoves();
+        for (int i = 0; i < moves.size(); i++) {
+            GameStatus fill = new GameStatus(s);
+            fill.movePiece(moves.get(i));
+            maxEval = Math.max(maxEval, minValor(fill, depth-1));
+        }
+        return maxEval;
     }
 
     /**
@@ -113,18 +127,34 @@ public class PlayerMiniMax implements IPlayer, IAuto {
 
         //Coin Parity Heuristic Value =
         //100 * (Max Player Coins - Min Player Coins ) / (Max Player Coins + Min Player Coins)
-
             return 1;
+           
         }
 
     public int calcula(CellType player, GameStatus s){
-        /*int heur = 0;
+        int heur = 0;
 
         int fitxes = s.getScore(player);
 
-        int moviments = (s.getMoves(player)).size;/*
+        ArrayList<Point> moves =  s.getMoves();
         
-        */
-        return 1;
+
+        int moviments = (moves).size();
+        
+        int corners = 0;
+        if(s.getPos(0,0) == player){
+            corners += 100;
+        }
+        if(s.getPos(0,7) == player){
+            corners += 100;
+        }
+        if(s.getPos(7,0) == player){
+            corners += 100;
+        }
+        if(s.getPos(7,7) == player){
+            corners += 100;
+        }
+
+        return heur;
     }
 }
